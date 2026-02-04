375c7ab907d192c12f3b34245e3b618e863a16da
andriihalchuk <galhukandrij@gmail.com>
2026-02-04 21:35:14 +0000

    made inventory toggle and simple interactions
    lol

---
+++ scenes/main.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+@onready var player: Player = $Player
+@onready var inventory_interface: Control = $UI/InventoryInterface
+
+func _ready() -> void:
+	player.toggle_inventory.connect(toggle_inventory_interface)
+	inventory_interface.set_player_inventory_data(player.inventory_data)
+
+func toggle_inventory_interface() -> void:
+	inventory_interface.visible = not inventory_interface.visible
+	
--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,9 @@ class_name Player
 
 var chunk_pos: Vector2i = Vector2i.ZERO
 @export var speed: float = 200.0
+@export var inventory_data: InventoryData
+
+signal toggle_inventory()
 
 func _process(delta: float) -> void:
 	if velocity.x > 0:
@@ -23,3 +26,6 @@ func _physics_process(delta: float) -> void:
 
 	# Update chunk position
 	chunk_pos = floor(position / (Globals.CHUNK_SIZE * Globals.TILE_SIZE * Globals.PPM))
+
+	if Input.is_action_just_pressed("inventory"):
+		toggle_inventory.emit()
