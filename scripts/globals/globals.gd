b03ec7bbdacbe5041dd422c01a8e1a2b6c83270d
danmarkowal <danmarkowal@gmail.com>
2026-02-04 15:31:07 +0000

    Simple player model

--- scripts/globals/globals.gd
+++ scripts/globals/globals.gd
@@ -1,5 +1,5 @@
 extends Node
 
 const CHUNK_SIZE: int = 16 # in tiles, chunks are square shaped
-const TILE_SIZE: int = 1 # in metres, tiles are also square shaped
+const TILE_SIZE: int = 2 # in metres, tiles are also square shaped
 const PPM: int = 64 # pixels per metre
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,12 @@ class_name Player
 var chunk_pos: Vector2i = Vector2i.ZERO
 @export var speed: float = 200.0
 
+func _process(delta: float) -> void:
+	if velocity.x > 0:
+		$Components.scale.x = 1
+	elif velocity.x < 0:
+		$Components.scale.x = -1
+
 func _physics_process(delta: float) -> void:
 	# Get input direction
 	var direction := Vector2.ZERO
