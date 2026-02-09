dd68ecf8c5026ce437d6a1e7bb1195f67475c2d7
andriihalchuk <galhukandrij@gmail.com>
2026-02-09 21:56:11 +0000

    inventory plus the chest interface
    simple inventory and chest interfaces

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ class_name Player
 var chunk_pos: Vector2i = Vector2i.ZERO
 @export var speed: float = 200.0
 @export var inventory_data: InventoryData
+@onready var shape_cast_2d: ShapeCast2D = $Camera2D/ShapeCast2D
 
 signal toggle_inventory()
 
@@ -27,5 +28,14 @@ func _physics_process(delta: float) -> void:
 	# Update chunk position
 	chunk_pos = floor(position / (Globals.CHUNK_SIZE * Globals.TILE_SIZE * Globals.PPM))
 
+	# Check if pressed Tab or E for inventory or chest
 	if Input.is_action_just_pressed("inventory"):
 		toggle_inventory.emit()
+	
+	if Input.is_action_just_pressed("interact"):
+		interact()
+
+# if either of rays collides with a chest, you can interact with it
+func interact() -> void:
+	if shape_cast_2d.is_colliding():
+		shape_cast_2d.get_collider(0).player_interact()
